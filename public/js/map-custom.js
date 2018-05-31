d902d2395818a68257a7b373a022e3405898afd8
viral <viralpatel.patel78@gmail.com>
2018-05-31 20:51:57 +0530

    Font Added

--- public/js/map-custom.js
+++ public/js/map-custom.js
@@ -15,8 +15,8 @@
                 img_pin = 'images/icons/location.png';
             }
             if (data_map_x == null || data_map_y == null) {
-                data_map_x = 40.007749;
-                data_map_y = -93.266572;
+                data_map_x = 13.0827;
+                data_map_y = 80.2707;
             }
             if (scrollwhell == null) {
                 scrollwhell = 0;
